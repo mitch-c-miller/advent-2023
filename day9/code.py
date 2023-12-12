b42c26d41548129dbbc9eba4c531dc7ab8b10a9b
Mitch Miller <mitch.miller@shopify.com>
2023-12-12 09:35:49 -0500

    day 9 setup

---
+++ day9/code.py
@@ -0,0 +1,23 @@
+from sys import path
+path.append('../advent2023')
+
+import utils
+from collections import Counter
+from enum import Enum
+
+def mirage(sensors: list[int]) -> int:
+    res = 0
+    return res
+
+
+if __name__ == '__main__':
+    data = utils.read_input(9, False)
+
+
+    # test answer: 
+    # prod answer: 
+    print(mirage(data))
+
+    # test answer: 
+    # prod answer: 
+    # print(mirage(hands, True))
